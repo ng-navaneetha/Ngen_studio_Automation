3a7dfd6ae881510231bdc945101a786c00e07690
ng-navaneetha <navaneetha.b@ngenux.com>
2025-09-19 19:48:57 +0530

    updated config file for json reporter

--- constants/insights_data.js
+++ constants/insights_data.js
@@ -47,12 +47,12 @@ export const WORKSPACE_DATA = {
     useCase: 'Insights'
   },
   INSIGHTSEnterpriseSearch: {
-    name: 'Tesla’s Transforming ESG into Impact MeasurementEnterprise Search Workspace',
+    name: 'Tesla’s Transforming ',
     description: 'Current ESG methodologies prioritize investment risk over real-world impact,    often misrepresenting companies’ environmental and social contributions. This     document critiques these flaws, advocating for a shift to measuring tangible positive outcomes,    like accurate use-phase emissions in automotive industries. It highlights Tesla’s    approach, emphasizing sustainable products, transparent data, and equitable workplaces.    By 2030, Tesla aims to sell 20 million EVs annually, significantly reducing CO2e emissions.    The report calls for stakeholder collaboration to redefine ESG as Impact, focusing    on lifecycle emissions, employee welfare, and ethical supply chains to drive meaningful    global change.',
     useCase: 'Insights'
   },
   QUERYEnterpriseSearch: {
-    name: 'acme_chatbot schema Query Enterprise Search Workspace',
+    name: 'acme_chatbot',
     description: 'The acme_chatbot schema in PostgreSQL supports ACME’s pharmaceutical operations with nine tables. demand-forecast predicts product demand for inventory planning. material-packing-tracker manages component inventory. otif and kpi_invoice_to_order_calc track order fulfillment and delivery KPIs. manuf-delay and batch-and-packing-tracker monitor production delays and batch timelines. manufacturing-assesses equipment utilization. doh calculates inventory days-on-hand for stock management. process-area-details evaluates manufacturing efficiency. Together, these tables enable supply chain optimization, production planning, and performance analysis for ACME’s pharmaceutical manufacturing and distribution processes.',
     useCase: 'Query'
   }
